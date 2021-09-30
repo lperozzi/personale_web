874ce462c398760eca99ecee77b9a52f07519986
Lorenzo <lorenzo.perozzi@gmail.com>
2021-09-30 19:53:39 +0200

    fixing os:image

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,96 +1,21 @@
-// const siteMetadata = {
-//   title: 'Geodata science blog',
-//   author: 'Lorenzo Perozzi',
-//   headerTitle: 'geomaap.io',
-//   description: 'A blog about geodata processing, modeling and visualization',
-//   language: 'en-us',
-//   siteUrl: 'https://personale-web.vercel.app',
-//   siteRepo: 'https://github.com/lperozzi/personale_web',
-//   siteLogo: '/static/images/logo.png',
-//   image: '/static/images/Lorenzo.jpg',
-//   socialBanner: '/static/images/twitter-card.png',
-//   email: 'lorenzo@geomaap.io',
-//   github: 'https://github.com/lperozzi',
-//   twitter: 'https://twitter.com/Twitter',
-//   facebook: 'https://facebook.com',
-//   youtube: 'https://youtube.com',
-//   linkedin: 'https://www.linkedin.com/in/lperozzi/',
-//   googlescholar: 'https://scholar.google.com/citations?user=LWLv6M8AAAAJ&hl=en',
-//   locale: 'en-US',
-//   analytics: {
-//     // supports plausible, simpleAnalytics or googleAnalytics
-//     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-//     simpleAnalytics: false, // true or false
-//     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
-//   },
-//   newsletter: {
-//     // supports mailchimp, buttondown, convertkit
-//     // Please add your .env file and modify it according to your selection
-//     provider: 'buttondown',
-//   },
-//   comment: {
-//     // Select a provider and use the environment variables associated to it
-//     // https://vercel.com/docs/environment-variables
-//     provider: 'giscus', // supported providers: giscus, utterances, disqus
-//     giscusConfig: {
-//       // Visit the link below, and follow the steps in the 'configuration' section
-//       // https://giscus.app/
-//       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-//       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-//       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-//       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-//       mapping: 'title', // supported options: pathname, url, title
-//       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-//       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-//       metadata: '0',
-//       // theme example: light, dark, dark_dimmed, dark_high_contrast
-//       // transparent_dark, preferred_color_scheme, custom
-//       theme: 'light',
-//       // theme when dark mode
-//       darkTheme: 'dark_dimmed',
-//       // If the theme option above is set to 'custom`
-//       // please provide a link below to your custom theme css file.
-//       // example: https://giscus.app/themes/custom_example.css
-//       themeURL: '',
-//     },
-//     utterancesConfig: {
-//       // Visit the link below, and follow the steps in the 'configuration' section
-//       // https://utteranc.es/
-//       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-//       issueTerm: '', // supported options: pathname, url, title
-//       label: '', // label (optional): Comment 💬
-//       // theme example: github-light, github-dark, preferred-color-scheme
-//       // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-//       theme: '',
-//       // theme when dark mode
-//       darkTheme: '',
-//     },
-//     disqusConfig: {
-//       // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
-//       shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
-//     },
-//   },
-// }
-
-// module.exports = siteMetadata
-
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Geodata science blog',
+  author: 'Lorenzo Perozzi',
+  headerTitle: 'geomaap.io',
+  description: 'A blog about geodata processing, modeling and visualization',
   language: 'en-us',
   siteUrl: 'https://personale-web.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/lperozzi/personale_web',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/Lorenzo.jpg',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'lorenzo@geomaap.io',
+  github: 'https://github.com/lperozzi',
   twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/lperozzi/',
+  googlescholar: 'https://scholar.google.com/citations?user=LWLv6M8AAAAJ&hl=en',
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics or googleAnalytics
@@ -114,7 +39,7 @@ const siteMetadata = {
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
+      mapping: 'title', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
@@ -122,7 +47,7 @@ const siteMetadata = {
       // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: 'dark_dimmed',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
@@ -148,3 +73,4 @@ const siteMetadata = {
 }
 
 module.exports = siteMetadata
+
