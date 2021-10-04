40b58e5f4bf3039b2a1dc3e5e760544218a9aef2
Lorenzo <lorenzo.perozzi@gmail.com>
2021-10-04 10:07:15 +0200

    googleanalytics out

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,7 +21,7 @@ const siteMetadata = {
     // supports plausible, simpleAnalytics or googleAnalytics
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    googleAnalyticsId: 'G-DKG8420KG1', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit
